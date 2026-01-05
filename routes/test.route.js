cbb8c4c565d5c9dfc038670ad74ad2c9cc156a2c
Jay Suthar <jay1357suthar@gmail.com>
2026-01-05 15:51:22 +0530

    Added test API route

---
+++ routes/test.route.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+    res.json({
+        success: true,
+        message: "Backend is working 🚀",
+    });
+});
+
+module.exports = router;
